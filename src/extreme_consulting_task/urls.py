31203113133b5697d94a205ba4f76981eb1e6f45
maddrum <maddrum9@gmail.com>
2021-02-11 17:08:13 +0200

    initial commit initialized django webapp initialized calculator app

---
+++ src/extreme_consulting_task/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'calculator/', include('calculator.urls', namespace='calculator')),
+    path('admin/', admin.site.urls),
+]
