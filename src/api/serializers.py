11839056450fdb17cc7c4a3329ef87e8fc119c1b
maddrum <maddrum9@gmail.com>
2021-02-11 23:07:12 +0200

    added math logic added validators

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from calculator.forms import CalculationForm
 
 
@@ -6,3 +7,7 @@ class InputSerializer(serializers.Serializer):
     number_a = serializers.FloatField()
     number_b = serializers.FloatField()
     operator = serializers.ChoiceField(choices=CalculationForm().OPERATOR_CHOICES)
+
+
+class ResultSerializer(serializers.Serializer):
+    result = serializers.FloatField()
--- src/api/views.py
+++ src/api/views.py
@@ -1,13 +1,27 @@
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
 from rest_framework.views import APIView
+
 from api import serializers
+from calculator.views import CalculatorView
 
 
 class RestCalculatorView(APIView):
     serializer_class = serializers.InputSerializer
 
     def get(self, request):
-        return Response('input number_a, number_b and operator')
+        return Response('Input number_a, number_b and operator')
 
     def post(self, request, *args, **kwargs):
-        return Response('The result is: ')
+        serializer = serializers.InputSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
+        result_serializer = serializers.ResultSerializer()
+        data = result_serializer.data
+        number_a = float(request.data.get('number_a'))
+        number_b = float(request.data.get('number_b'))
+        operator = request.data.get('operator')
+        calculation_result = CalculatorView().calculate_result(a=number_a, b=number_b, operator=operator)
+        # handle ok result
+        data['result'] = calculation_result
+        return Response(data, status=HTTP_200_OK)
