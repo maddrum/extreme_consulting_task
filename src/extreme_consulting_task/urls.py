a29c6eb9fc541a88fbed2b34a6e5a949459fc0b2
maddrum <maddrum9@gmail.com>
2021-02-11 21:39:57 +0200

    urls resolve path update

--- src/extreme_consulting_task/urls.py
+++ src/extreme_consulting_task/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'calculator/', include('calculator.urls', namespace='calculator')),
+    url(r'^calculator/', include('calculator.urls', namespace='calculator')),
     path('admin/', admin.site.urls),
 ]
