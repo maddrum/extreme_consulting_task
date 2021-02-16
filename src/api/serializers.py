a2957b6bfa504fe03281a4dd65526a89f129ad69
maddrum <maddrum9@gmail.com>
2021-02-16 13:06:13 +0200

    added api division by zero error handling

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -8,6 +8,11 @@ class InputSerializer(serializers.Serializer):
     number_b = serializers.FloatField()
     operator = serializers.ChoiceField(choices=CalculationForm().OPERATOR_CHOICES)
 
+    def validate(self, data):
+        if data['operator'] == '/' and data['number_b'] == 0:
+            raise serializers.ValidationError("Division by zero")
+        return data
+
 
 class ResultSerializer(serializers.Serializer):
     result = serializers.FloatField()
